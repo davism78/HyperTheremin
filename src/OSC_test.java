f8295496c42b3c0376fbd781c74800571946b940
David Bjelland <dkb8@uw.edu>
2014-01-23 14:46:48 -0800

    It Fucking Works, what else do you want?

---
+++ src/OSC_test.java
@@ -0,0 +1,35 @@
+import com.illposed.osc.*;
+import java.net.*;
+
+public class OSC_test {
+	public static void main(String[] args) throws UnknownHostException, SocketException, java.io.IOException, InterruptedException {
+	    // The IP Address of the server (or listener) we would like to send to
+	    // For testing we will use the `getLocalHost` function to send to our
+	    // machine, but for using with different physical devices, use something
+	    // like the following commented out line
+	    //InetAddress remoteIP  = InetAddress.getByName("192.168.1.1");
+	    InetAddress remoteIP = InetAddress.getLocalHost();
+	    
+	    // The port that the server we are sending to is listening on
+	    int remotePort = 8000;
+	    
+	    // Bring the IP Address and port together to form our OSC Sender
+	    OSCPortOut sender = new OSCPortOut(remoteIP, remotePort);
+	    
+	    // The address to send our message to
+	    String address1 = "/note";
+	    
+	    // An array of objects that are our values we would like to send
+	    Object values1[] = new Object[2];
+	    values1[0] = new Integer(440);
+	    values1[1] = new Integer(75);
+	    
+	    // Bring the address and values together to form an OSCMessage
+	    OSCMessage message1 = new OSCMessage(address1, values1);
+	   
+	    // Send each message
+	    System.out.printf("Sending message1 to %s:%s at %s\n", remoteIP, remotePort, message1.getAddress());
+	    sender.send(message1);
+	  }
+	}
+	
--- src/ThereminListener.java
+++ src/ThereminListener.java
@@ -1,14 +1,46 @@
+import com.illposed.osc.OSCMessage;
+import com.illposed.osc.OSCPortOut;
 import com.leapmotion.leap.*;
 import com.leapmotion.leap.Gesture.State;
+import com.illposed.osc.*;
 
+import java.net.*;
 import java.io.IOException;
+import java.net.InetAddress;
 
 public class ThereminListener extends Listener {
 	private static final double SCALE = 30.0;
 	private static final double OFFSET = 25.0;
 	private static final double CZERO = 16.25;
+    
+	//net stuff
+	private InetAddress remoteIP;
+    private int remotePort = 8000;
+    String address1 = "/note";
+    OSCPortOut sender;
+
 	
     public void onInit(Controller controller) {
+    	 // The IP Address of the server (or listener) we would like to send to
+	    // For testing we will use the `getLocalHost` function to send to our
+	    // machine, but for using with different physical devices, use something
+	    // like the following commented out line
+	    //InetAddress remoteIP  = InetAddress.getByName("192.168.1.1");
+    	try {
+			remoteIP = InetAddress.getLocalHost();
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	    // The port that the server we are sending to is listening on
+	    
+		try {
+			sender = new OSCPortOut(remoteIP, remotePort);
+		} catch (SocketException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
         System.out.println("Initialized");
     }
 
@@ -41,6 +73,17 @@ public class ThereminListener extends Listener {
             	float Yval = v.getY();
             	double tone = CZERO * Math.pow(2, ((Yval - OFFSET) / SCALE));
             	System.out.println(tone);
+            	Object values1[] = new Object[2];
+            	values1[0] = new Float(tone);
+        	    values1[1] = new Integer(75);
+        	    OSCMessage message1 = new OSCMessage(address1, values1);
+        	    try {
+					sender.send(message1);
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+
             }
     	}
     }
