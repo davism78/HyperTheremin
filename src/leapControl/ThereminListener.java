a27abc0a1f8df27c80331b57028873b1ed3bc29f
davism78 <davism78@uw.edu>
2014-01-30 17:12:12 -0800

    Added preliminary user controlled level.  Refactored onFrame method somewhat.

--- src/leapControl/ThereminListener.java
+++ src/leapControl/ThereminListener.java
@@ -1,6 +1,7 @@
 package leapControl;
 
 import com.leapmotion.leap.Controller;
+import com.leapmotion.leap.Finger;
 import com.leapmotion.leap.FingerList;
 import com.leapmotion.leap.Frame;
 import com.leapmotion.leap.Hand;
@@ -29,7 +30,31 @@ public class ThereminListener extends Listener {
     public void onExit(Controller controller) {
         System.out.println("Exited");
     }
-
+    
+    /*
+     * Evaluate fingers position into a double value representing a pitch
+     */
+    private double getTone(Finger finger) {
+       	Vector v = finger.tipPosition();
+       	float Yval = v.getY();
+       	double tone = CZERO * Math.pow(2, ((Yval - OFFSET) / SCALE));
+       	return tone;
+    }
+    
+    /*
+     * Evaluate a fingers position into an int value representing volume level
+     * 
+     * TODO this method need much more thought out implementation
+     */
+    private int getLevel(Finger finger) {
+       	Vector v = finger.tipPosition();
+       	float Yval = v.getY();
+       	int level = (int)Yval;
+       	if (Yval > 100.0)
+       		return 100;
+       	return level;
+    }
+    
     /*
      * This method is intended to transform a finger point position into an
      * audio frequency that can be used as input to an oscillator.
@@ -38,20 +63,43 @@ public class ThereminListener extends Listener {
      */
     public void onFrame(Controller controller) {
     	Frame frame = controller.frame();
-    	if (!frame.hands().isEmpty()) {
+    	int level = 0;
+    	double tone = 0;
+    	
+    	System.out.println("#Hands: " + frame.hands().count());
+    	// Process hands
+    	switch (frame.hands().count()) {
+    	case 0:
+    		break;
+    	case 1:
+    		// Use constant level
+    		level = 75;
     		Hand hand = frame.hands().get(0);
     		FingerList fingers = hand.fingers();
-            if (!fingers.isEmpty()) {
-            	Vector v = fingers.get(0).tipPosition();
-            	float Yval = v.getY();
-            	double tone = CZERO * Math.pow(2, ((Yval - OFFSET) / SCALE));
-            	System.out.println(tone);
-            	
-            	if(!pitchConnection.sendPitch(tone, 75)){
-            		System.out.println("ERROR: message did not send");
-            	}
-            }
+    		if (!fingers.isEmpty()){
+    			// should be a loop over all fingers
+    			tone = getTone(fingers.get(0));
+    		}
+    		break;
+    	default:  // TODO handle more than two hands
+    		FingerList levels = frame.hands().get(0).fingers();
+    		FingerList tones = frame.hands().get(1).fingers();
+    		if (!levels.isEmpty()){
+    			//TODO level should be determined by overall hand position, not based on finger
+    			level = getLevel(levels.get(0));
+    		}
+    		if (!tones.isEmpty()){
+    			//TODO should be a loop over all fingers
+    			tone = getTone(tones.get(0));
+    		}
+    		break;
     	}
-    }
-    
+
+    	System.out.println(tone);
+    	if(!pitchConnection.sendPitch(tone, level)){
+    		System.out.println("ERROR: message did not send");
+    	}  
+    } 
 }
+
+
