f9f12ef11a446b936de64650cf13b216a688911a
Joseph Godlewski <joe3701@gmail.com>
2014-02-03 12:15:59 -0800

    Extracted the graphics code to the Graphics model and added resizing of the display window.

--- src/Theremin.java
+++ src/Theremin.java
@@ -1,30 +1,28 @@
 
+import graphics.GraphicsModel;
 import leapControl.ThereminListener;
 
 import org.lwjgl.LWJGLException;
-import org.lwjgl.opengl.Display;
-import org.lwjgl.opengl.DisplayMode;
 
 import com.leapmotion.leap.Controller;
 
 
 class Theremin {
 	public static void main(String[] args) throws LWJGLException {
-        // Create a sample listener and controller
-        ThereminListener listener = new ThereminListener();
-        Controller controller = new Controller();
+		// Create a sample listener and controller
+		ThereminListener listener = new ThereminListener();
+		Controller controller = new Controller();
+		GraphicsModel graphics = new GraphicsModel();
 
-        // Have the sample listener receive events from the controller
-        controller.addListener(listener);
-        
-        //LWJGL
-        Display.setDisplayMode(new DisplayMode(400, 400));
-        Display.create();
-        while(!Display.isCloseRequested()) {
-        	Display.update();
-        }
-        controller.removeListener(listener);
-        Display.destroy();
-    }
+		// Have the sample listener receive events from the controller
+		controller.addListener(listener);
+
+		// run the graphics
+		graphics.runDisplay();
+
+		// cleanup
+		controller.removeListener(listener);
+		graphics.cleanup();
+	}
 
 }
--- src/graphics/GraphicsModel.java
+++ src/graphics/GraphicsModel.java
@@ -1,5 +1,79 @@
 package graphics;
 
-public class GraphicsModel {
+import static org.lwjgl.opengl.GL11.*;
 
+import org.lwjgl.LWJGLException;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
+
+public class GraphicsModel {
+	private static final String TITLE = "Theremin";
+	private static final boolean FULL_SCREEN = false;
+	private static final int WIN_HEIGHT = 600;
+	private static final int WIN_WIDTH = 800;
+	
+	private boolean isCreated;
+	
+	public GraphicsModel() {
+		setupDisplay();
+		
+	}
+	
+	/**
+	 * Sets up the display to the default dimensions
+	 */
+	private void setupDisplay() {
+		try {
+			Display.setTitle(TITLE);
+			Display.setDisplayMode(new DisplayMode(WIN_WIDTH, WIN_HEIGHT));
+			Display.setResizable(true);
+			Display.setFullscreen(FULL_SCREEN);
+			Display.create();
+			isCreated = true;
+			glClearColor(0.0F, 0.0F, 0.0F, 0.0F);
+			resize();
+			
+		} catch (LWJGLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * resizes the viewport on the Display to match the dimensions of the Display.
+	 */
+	private void resize() {
+		glViewport(0, 0, Display.getWidth(), Display.getHeight());
+		
+		
+	}
+
+	/**
+	 * Starts up the display in a loop until the user wishes to terminate the application.
+	 * When the method returns, the user will have requested to close.
+	 */
+	public void runDisplay() {
+		while(!Display.isCloseRequested()) {
+			
+			if(Display.wasResized()) {
+				resize();
+			}
+			
+			// Clears the screen
+			glClear(GL_COLOR_BUFFER_BIT);
+			
+        	Display.update();
+        	Display.sync(60);
+        }
+	}
+	
+	/**
+	 * Cleans up the Display used in the Application.
+	 */
+	public void cleanup() {
+		if(isCreated) {
+			Display.destroy();
+			isCreated = false;
+		}
+	}
+	
 }
