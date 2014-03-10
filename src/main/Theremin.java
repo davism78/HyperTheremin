61b4474a12f2fec5e16e2078731ca20654b74e09
davism78 <davism78@uw.edu>
2014-03-09 16:27:27 -0800

    changed pd port back to 8000
    added PD autostart function.  needs to be generalized for any machine

--- src/main/Theremin.java
+++ src/main/Theremin.java
@@ -2,6 +2,9 @@ package main;
 
 import graphics.GraphicsModel;
 import graphics.GraphicsView;
+
+import java.io.IOException;
+
 import leapControl.ThereminListener;
 
 import org.lwjgl.LWJGLException;
@@ -17,15 +20,14 @@ public class Theremin {
 	private static ThereminListener listener;
 	private static boolean clean = false;
 	
+	private static Process PDSubprocess;
+	
 	public static void main(String[] args) throws LWJGLException {
 		// Setup the display
 		GraphicsModel graphics = new GraphicsModel();
 		gui = new GraphicsView(graphics);
 		
-		// TODO: auto start PD with our pd file
-		// we can use java.io.runtime:
-		// Runtime rt = Runtime.getRuntime();
-		// Process ps = rt.exec("path/to/pd.exe path/to/src.pd");
+		//launchPuredata();
 		
 		// Create a theremin listener and controller
 		listener = new ThereminListener(graphics);
@@ -47,10 +49,40 @@ public class Theremin {
 		}
 	}
 
+	private static void launchPuredata() {
+		// auto start PD with our pd file
+		// we can use java.io.runtime:
+		Runtime rt = Runtime.getRuntime();
+		
+		String url = Theremin.class.getResource("").getPath();
+
+		System.out.println(url);
+		
+		String pdsrcurl = url.substring(1) + "..\\..\\pd_src\\theremin.pd";
+		String pdurl = url + "../../../../WindowsFolders/Pd-0.43.4-extended-windowsxp-i386/Pd-0.43.4-extended/bin/pd.exe";
+		
+		try {
+			PDSubprocess = rt.exec(pdurl + " " + pdsrcurl);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
 	public static void exit() {
 		listener.cleanup();
 		controller.removeListener(listener);
 		gui.cleanup();
+		
+		if (PDSubprocess != null){
+			try {
+				PDSubprocess.exitValue();
+			} catch (IllegalThreadStateException e) {
+				PDSubprocess.destroy();
+				System.out.println("PD destroyed");
+			} 
+			// if exception not thrown, process has already terminated
+		}
 		clean = true;
 	}
 }
